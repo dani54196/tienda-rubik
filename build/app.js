a941b7fd24c880428f71152aeedac6fca14b7367
dani54196 <dani54196@gmail.com>
2022-05-16 21:43:49 -0300

    docker connect to mysql

--- build/app.js
+++ build/app.js
@@ -4,7 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 // config
 app.set("port", process.env.PORT || 3000);
+app.use((0, morgan_1.default)("dev"));
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
 // config
 app.set("port", process.env.PORT || 3000);
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 export default app;
